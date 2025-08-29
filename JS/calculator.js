6d7a5da52d63ed2d27b2d6335269ef08a71b01c8
alvarogarcia1994 <alvarogb.barcelona94@gmail.com>
2025-08-29 11:33:18 +0200

    Basic arithmetic operations and warning by dividing by zero finished

--- JS/calculator.js
+++ JS/calculator.js
@@ -1,9 +1,9 @@
 //Arithmetic operations stored in an object
 const arithmetic_Operations = {
     '+': (a, b) => a + b,
-    '-': (a, b) => b - a,
+    '-': (a, b) => a - b,
     '*': (a, b) => a * b,
-    '/': (a, b) => b / a,
+    '/': (a, b) => b !== 0 ? a / b : 'Zero division error',
     '%': (a, b) => a % b
 };
 
@@ -12,7 +12,6 @@ let currentInput = '';
 let previousInput = '';
 let selectedOperation = '';
 
-
 //Variable block of element buttons to apply events with addEventlistener further
 const numericKeys = document.querySelectorAll('.numeric_key');
 const arithmeticOperations = document.querySelectorAll('.arithmetic-operation');
@@ -20,6 +19,43 @@ const clearKey = document.querySelector('#clear');
 const toggleKey = document.querySelector('#light-n-dark-mode');
 const decimalKey = document.querySelector('#decimal');
 const equalKey = document.querySelector('#equal');
+const displayElement = document.querySelector('input[type="text"]');
+
+//Event handle to the previous block of element buttons
+//numericKeys group
+numericKeys.forEach(btn => {
+    btn.addEventListener('click', () => {
+        giveNumber(btn.textContent.trim());
+    });
+})
+
+//arithmeticOperations group
+arithmeticOperations.forEach(op => {
+    op.addEventListener('click', () =>{
+        appendOperation(op.textContent.trim())
+    })
+})
+
+//clearKey
+clearKey.addEventListener('click', function(){
+    clearDisplay();
+});
+
+//toggleKey
+toggleKey.addEventListener('click', function(){
+    toggleMode();
+})
+
+//decimalKey
+decimalKey.addEventListener('click', function(){
+    addDecimal();
+})
+
+//equalKey
+equalKey.addEventListener('click', function(){
+    performOperation()
+})
+
 
 
 //Config array to glow the buttons of the calculator
@@ -40,14 +76,96 @@ config.forEach(({ selector, glowClass }) => {
     
     //Nested forEach to apply and or remove glow class
     elements.forEach(el => {
+        
+        //Mouseenter event
         el.addEventListener('mouseenter', () => {
+            //Message in the console to indicate the application of glowclass
             console.log(`mouseenter: adding ${glowClass}`);
             el.classList.add('hover-animated', glowClass);
         });
 
+        //Mouseleave event
         el.addEventListener('mouseleave', () => {
+            //Same procedure but in reverse
             console.log(`mouseleave: removing ${glowClass}`);
             el.classList.remove('hover-animated', glowClass);
         });
     });
 });
+
+//Function to clear the display
+function clearDisplay() {
+    currentInput = '';
+    previousInput = '';
+    currentOperation = '';
+    updateDisplay('');
+}
+
+//Function to toggle between light and dark mode
+function toggleMode(){
+    displayElement.classList.toggle('dark-mode');
+}
+
+//Function to append a number
+function giveNumber(number) {
+    currentInput += number;
+    updateDisplay(currentInput);
+}
+
+//Function to update the display
+function updateDisplay(value) {
+    displayElement.value = value;
+}
+
+//Funtion to add decimal values
+function addDecimal() {
+    if (!currentInput.includes('.')) {
+        currentInput += currentInput === '' ? '0.' : '.';
+        updateDisplay(currentInput);
+    }
+}
+
+//Function to handle operation
+function appendOperation(operation) {
+
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        performOperation(); 
+    }
+
+    //Normalizing "x" symbol to => "*" symbol defined as multiplying in arithmetic_Operations object.
+    if (operation === 'x') operation = '*';
+
+
+    selectedOperation = operation;
+    previousInput = currentInput;
+    currentInput = '';
+    updateDisplay(`${previousInput} ${selectedOperation}`);
+
+}
+
+//Function to handle each operation
+function performOperation() {
+    if (previousInput === '' || currentInput === '' || !selectedOperation) return;
+
+    let prev = parseFloat(previousInput);
+    let current = parseFloat(currentInput);
+
+    const operation = arithmetic_Operations[selectedOperation];
+    if (!operation) return;
+
+    let result = operation(prev, current);
+
+    if (result === 'Zero division error') {
+        updateDisplay(result);
+        currentInput = '';
+        previousInput = '';
+        selectedOperation = '';
+        return;
+    }
+
+    currentInput = result.toString();
+    previousInput = '';
+    selectedOperation = '';
+    updateDisplay(currentInput);
+}
